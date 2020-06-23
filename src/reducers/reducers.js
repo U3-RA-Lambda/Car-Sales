274cc45470e69e25e0449b9b0b0b9bcb64acfcb5
Michelle Hey <miscmiche@gmail.com>
2020-06-23 19:44:11 -0400

    MVP not met, functionality not improved upon yet

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,19 +1,53 @@
 import React from 'react';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
-export default const myReducer = (state = initialState, action) {
-    const newState = {...state};
 
-    if(action.type === 'ADD'){
-        newState.something +=1;
-    }
+export const initialState = [
+    {
+    additionalPrice: 0, // what to update
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  }
+]
 
-    return newState;
-}
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_ITEM':
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice + action.payload.price, 
+        car: {
+          ...state.car,
+          features: [
+            ...state.car.features, 
+            action.payload 
+          ]
+        }
+      }
 
-const store = createStore(myReducer);
-
-store.dispatch({type: 'ADD'});
-store.dispatch({type: 'SUBTRACT'});
-
-console.log(store.getState());
+    case 'REMOVE_ITEM':
+      return {
+        ...state, 
+        additionalPrice: state.additionalPrice - action.payload.price, 
+        car: {
+          ...state.car,
+          features: [
+            ...state.car.features.filter(item => item.id !== action.payload.id)
+          ]
+        }
+      }
+    default: 
+      return state
+  }
+} 
